08dbdc24cf409b54738fb601bd7d819366ead45f
CP14 <CP14@CP14-PC>
2014-11-18 12:48:09 -0500

    result

---
+++ prueba_01/src/prueba_01/ZAMBRANO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba_01;
+
+/**
+ *
+ * @author CP14
+ */
+public class ZAMBRANO {
+    int a =8;
+        int b= 12;
+           
+        
+}
